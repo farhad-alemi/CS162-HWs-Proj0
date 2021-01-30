04b56465dcb7910deae68e03b32211cff7c2ce81
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-01-30 07:47:02 +0000

    limits debugged

--- hw0/limits.c
+++ hw0/limits.c
@@ -3,9 +3,14 @@
 
 int main() {
   struct rlimit lim;
-  printf("stack size: %ld\n", (long) RLIMIT_STACK);
-  printf("process limit: %ld\n", (long) RLIMIT_NPROC);
-  printf("max file descriptors: %ld\n", (long) RLIMIT_NOFILE - 1);
+  getrlimit (RLIMIT_STACK, &lim);
+  printf("stack size: %ld\n", lim.rlim_cur);
+
+    getrlimit (RLIMIT_NPROC, &lim);
+    printf("process limit: %ld\n", (long) lim.rlim_cur);
+
+    getrlimit (RLIMIT_NOFILE, &lim);
+    printf("max file descriptors: %ld\n", (long) lim.rlim_cur);
   return 0;
 }
 
